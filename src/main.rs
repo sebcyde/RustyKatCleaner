0349bde523047151e43c10ac73cdcd87aaa020d9
Sebastian <sebcyde@hotmail.com>
2023-07-26 16:25:38 +0100

    started home converter

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
 mod helper_functions;
+mod home_converter;
 use std::fs;
 
 #[tokio::main]
 async fn main() {
-    use crate::helper_functions::helper_functions::*;
+use crate::helper_functions::helper_functions::*;
+    use crate::home_converter::home_converter::*;
 
-    let dir_dirty: &str =
-        "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports";
-    let _dir_clean: &str =
-        "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports";
-    let _dir_logs: &str = "C:/Users/sebastian.cyde/Documents/Other/CleanerLogs";
-
-    // Filtering files to be transformed
-    let mut dirty_file_paths: &mut Vec<fs::DirEntry> = &mut Vec::new();
-    let mut file_paths: &mut Vec<String> = &mut Vec::new();
-
-    let path_result = get_dir_file_paths(dir_dirty, &mut dirty_file_paths).await;
-    result_checker(path_result).await;
-
-    let mut files: &mut Vec<fs::DirEntry> = &mut dirty_file_paths;
-    let filtered_directories = filter_dirty_directories(&mut files, &mut file_paths).await;
-    println!("Filtered Directories: {:?}", filtered_directories);
+    home_converter().await;
 }
---
+++ src/work_converter.rs
@@ -0,0 +1,9 @@
+pub mod work_converter {
+    pub async fn work_converter() {
+        let dir_dirty: &str =
+    "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports";
+        let _dir_clean: &str =
+    "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports";
+        let _dir_logs: &str = "C:/Users/sebastian.cyde/Documents/Other/CleanerLogs";
+    }
+}
