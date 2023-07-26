c51332069eda3a605734d400ad2ea9d0f24cfe21
sebastian <sebcyde@hotmail.com>
2023-07-26 10:32:03 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    // const currentDate = new Date();
+    // const day = String(currentDate.getUTCDate()).padStart(2, "0");
+    // const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
+    // const year = currentDate.getUTCFullYear();
+    // const formattedDate = `${day}${month}${year}`;
+
+    // const Main = async () => {
+    //   const location = await GetInput("Work or Home? ");
+
+    //   location.toLowerCase() == "work"
+    //     ? await WorkConverter(
+    //         "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports",
+    //         "C:/Users/sebastian.cyde/Documents/Other/KatReports",
+    //         `C:/Users/sebastian.cyde/Documents/Other/CleanerLogs`,
+    //         formattedDate
+    //       )
+}
